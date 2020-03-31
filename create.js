bd2cfa4ab40edd1453d4d0633c5f308757ad0d64
LeFranck <fralfarouc@gmail.com>
2020-03-31 09:12:17 -0300

    intended mistake?

--- create.js
+++ create.js
@@ -1,14 +1,12 @@
 import uuid from "uuid";
-import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export function main(event, context, callback)
-{
+export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    Item:{
+    Item: {
       userid: event.requestContext.identity.cognitoIdentityId,
       noteid: uuid.v1(),
       content: data.content,
@@ -16,27 +14,12 @@ export function main(event, context, callback)
       createdat: Date.now()
     }
   };
-  dynamoDb.put(params, (error, data) => {
-    console.log(params);
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({status: false})
-      };
-      console.log(error);
-      callback(null, response);
-      return;
-    }
-    const response = {
-      statusCode:200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 }
