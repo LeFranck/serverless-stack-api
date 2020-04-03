b6fe5a15e58f02d6f9dfa93713b5a7aee489edb2
LeFranck <fralfarouc@gmail.com>
2020-04-03 07:45:46 -0300

    Adding Serverless API (gateaway errors too)

--- billing.js
+++ billing.js
@@ -2,7 +2,7 @@ import stripePackage from "stripe";
 import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context) {
+export async function main(event, context){
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
   const description = "Scratch charge";
